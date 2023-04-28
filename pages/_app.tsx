0d8f86cb2bc0e1bdf2f9f5c4849eede83d6e7b03
gmdmsdud <ley690qw@naver.com>
2023-04-28 13:17:33 +0900

    등록

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import "antd/dist/antd.css";
+import { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import Layout from "../src/components/commons/layout/";
+import ApolloSetting from "../src/components/commons/apollo";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloSetting>
+      <>
+        <Global styles={globalStyles} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    </ApolloSetting>
+  );
+}
+
+export default MyApp;
