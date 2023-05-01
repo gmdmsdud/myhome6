a5a5942a6dc0a8b1401967052d6f4dfea6620833
gmdmsdud <ley690qw@naver.com>
2023-05-01 15:26:19 +0900

    등록

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -11,7 +11,7 @@ interface IApolloSettingProps {
 }
 export default function ApolloSetting(props: IApolloSettingProps) {
   const uploadLink = createUploadLink({
-    uri: "https://backend10.codebootcamp.co.kr/graphql",
+    uri: "https://backend11.codebootcamp.co.kr/graphql",
   });
 
   const client = new ApolloClient({
