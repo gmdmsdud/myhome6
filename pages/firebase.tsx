000fad98c28dc024106d767f541bcd89d7406794
gmdmsdud <ley690qw@naver.com>
2023-05-01 19:59:04 +0900

    등록

--- pages/firebase.tsx
+++ pages/firebase.tsx
@@ -1,35 +1,35 @@
-import {
-    collection,
-    getFirestore,
-    getDocs,
-    addDoc,
-  } from "firebase/firestore/lite";
-import { firebaseApp } from "./_app";
+// import {
+//     collection,
+//     getFirestore,
+//     getDocs,
+//     addDoc,
+//   } from "firebase/firestore/lite";
+// import { firebaseApp } from "./_app";
 
-export default function FirebasePage() {
-    const onClickSubmit =() => {
-       const board = collection(getFirestore(firebaseApp),"board")
-       void addDoc(board, {
-        writer: "철수",
-        title: "제목입니다.",
-     contents:"내용입니다."
-       })
-    }
+// export default function FirebasePage() {
+//     const onClickSubmit =() => {
+//        const board = collection(getFirestore(firebaseApp),"board")
+//        void addDoc(board, {
+//         writer: "철수",
+//         title: "제목입니다.",
+//      contents:"내용입니다."
+//        })
+//     }
 
-    const onClickFetch =async() => {
-        const board = collection(getFirestore(firebaseApp),"board")
-       const result = await getDocs(board)
-       const datas = result.docs.map(el => el.data()
-       )
-       console.log(datas)
-    }
+//     const onClickFetch =async() => {
+//         const board = collection(getFirestore(firebaseApp),"board")
+//        const result = await getDocs(board)
+//        const datas = result.docs.map(el => el.data()
+//        )
+//        console.log(datas)
+//     }
 
-    return (
-        <>
-        <button onClick ={onClickSubmit}> 등록하기 </button>
-        <button onClick ={onClickFetch}> 조회하기 </button>
-        </>
+//     return (
+//         <>
+//         <button onClick ={onClickSubmit}> 등록하기 </button>
+//         <button onClick ={onClickFetch}> 조회하기 </button>
+//         </>
             
         
-    )
-}
+//     )
+// }
